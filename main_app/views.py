8a4c9e7ad90b2b1eea9b46eff49edef9b4ca35a9
Andrea <andrearewards@hotmail.com>
2023-05-26 14:01:19 -0400

    create puppies through bit.io, create an admin page

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
+from .models import Puppy
 
-# Add the following import
-from django.http import HttpResponse
-
-# Add the Cat class & list and view function below the imports
-class Puppy:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-puppies = [
-  Puppy('Emi', 'Aussie mix', "Doesn't like when you stare at her", 2),
-  Puppy('April', 'mix', 'Eats more than she should', 8),
-  Puppy('Lola', 'malteese', 'Happy fluff ball.', 5),
-  Puppy('Jasper', 'malteese', 'Angel', 12)
-]
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 # Add new view
 def puppy_index(request):
+  puppies = Puppy.objects.all()
   return render(request, 'puppies/index.html', { 'puppies': puppies })
